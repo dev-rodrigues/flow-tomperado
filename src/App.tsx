127d7edada5bdd81c5485023537b496280b1eeb4
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2023-03-28 23:45:39 -0300

    link elements on board

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,67 @@
+import ReactFlow, {
+    addEdge,
+    Background,
+    Connection,
+    ConnectionMode,
+    Controls,
+    Node,
+    useEdgesState,
+    useNodesState
+} from "reactflow";
+import { zinc } from "tailwindcss/colors"
+
+import 'reactflow/dist/style.css';
+
+import {Square} from "./components/node/Square";
+import {useCallback} from "react";
+
+const NODE_TYPES = {
+    square: Square
+}
+
+const INITIAL_ELEMENTS = [
+    {
+        id: crypto.randomUUID(),
+        type: 'square',
+        position: {x: 200, y: 400},
+        data: {}
+    },
+    {
+        id: crypto.randomUUID(),
+        type: 'square',
+        position: {x: 1000, y: 400},
+        data: {}
+    }
+] satisfies Node[]
+
 function App() {
-  return <>
-        <h1>React Typescript</h1>
-  </>
+
+    const [edges, setEdges, onEdgesChange] = useEdgesState([])
+    const [nodes, setNodes, onNodesChange] = useNodesState(INITIAL_ELEMENTS)
+
+    const onConnect = useCallback((connection: Connection) => {
+        return setEdges(edges => addEdge(connection, edges))
+    }, [])
+
+    return (
+        <div className="w-screen h-screen">
+            <ReactFlow
+                nodeTypes={NODE_TYPES}
+                nodes={INITIAL_ELEMENTS}
+                connectionMode={ConnectionMode.Loose}
+                edges={edges}
+                onConnect={onConnect}
+                onEdgesChange={onEdgesChange}
+            >
+                <Background
+                    gap={12}
+                    size={2}
+                    color={zinc[200]}
+                />
+                <Controls />
+            </ReactFlow>
+        </div>
+    )
 }
 
 export default App
