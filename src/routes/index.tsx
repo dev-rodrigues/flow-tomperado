85cd9a22369b33d51c473476224ff57bbd8fc8d7
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2023-04-03 23:40:49 -0300

    add board

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
 import {Route, Switch} from "react-router-dom";
 import Index from "../pages/index";
 
+import Board from "../pages/board";
+
 
 export default function Routes() {
     return (
         <Switch>
             <Route path="/home" component={Index}/>
+            <Route path="/board" component={Board}/>
         </Switch>
     )
 }
